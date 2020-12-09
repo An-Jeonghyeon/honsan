21848748393669b18d4cfa44e2f6d5c43a83c2ea
AN-JEONGHYEON <AN-JEONGHYEON@DESKTOP-HVUOUT6>
2020-12-09 21:29:51 +0900

    중간 수정.. 폰트.. 부트스트랩 막기..

--- final/src/main/java/com/sp/app/bbs/BoardService.java
+++ final/src/main/java/com/sp/app/bbs/BoardService.java
@@ -13,4 +13,18 @@ public interface BoardService {
 	public Board nextReadBoard(Map<String, Object> map);
 	public void updateBoard(Board dto, String pathname) throws Exception;
 	public void deleteBoard(int num, String pathname, String userId) throws Exception;
+
+	public void insertBoardLike(Map<String, Object> map) throws Exception;
+	public int boardLikeCount(int num);
+	
+	public void insertReply(Reply dto) throws Exception;
+	public List<Reply> listReply(Map<String, Object> map);
+	public int replyCount(Map<String, Object> map);
+	public void deleteReply(Map<String, Object> map) throws Exception;
+	
+	public List<Reply> listReplyAnswer(int answer);
+	public int replyAnswerCount(int answer);
+	
+	public void insertReplyLike(Map<String, Object> map) throws Exception;
+	public Map<String, Object> replyLikeCount(Map<String, Object> map);
 }
--- final/src/main/webapp/WEB-INF/views/layout/layout_login.jsp
+++ final/src/main/webapp/WEB-INF/views/layout/layout_login.jsp
@@ -10,9 +10,11 @@
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <!-- 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">	 
+     -->
 
     <script src="${pageContext.request.contextPath}/resources/js/layout_login.js"></script>
       
