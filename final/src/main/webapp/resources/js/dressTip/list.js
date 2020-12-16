33e727b6e6af928de35f719ef6789485bc10c76f
ikyoungtae <ikyoungtae@macbook-pro>
2020-12-16 21:12:11 +0900

    c-ba

--- final/src/main/webapp/resources/js/dressTip/list.js
+++ final/src/main/webapp/resources/js/dressTip/list.js
@@ -1,14 +1,16 @@
 $(function(){
     
     $(".dress-TipMenu1").click(function(){
-        $(".dress-TipMenuBody1").css("background-color","#ededed80");
+    	$(".dress-TipMainShowArticleBody").css("background-color","#ededed00")
+        $(".dress-TipMenuBody1").css("background-color","#ededed00");
         $(".dress-InnerImg1>a >span").css("background-color","#ededed80")
         $(".dress-InnerImg2>a >span").css("background-color","rgba(192, 226, 255, 0.523)")
         $(".dress-TipMenuBody2").css("background-color","rgba(192, 226, 255, 0.523)")
         
     })
     $(".dress-TipMenu2").click(function(){
-        $(".dress-TipMenuBody2").css("background-color","#ededed80")
+    	$(".dress-TipMainShowArticleBody").css("background-color","#ededed00")
+        $(".dress-TipMenuBody2").css("background-color","#ededed00")
         $(".dress-InnerImg2>a >span").css("background-color","#ededed80")
         $(".dress-InnerImg1>a >span").css("background-color","rgba(192, 226, 255, 0.523)")
         $(".dress-TipMenuBody1").css("background-color","rgba(192, 226, 255, 0.523)")
