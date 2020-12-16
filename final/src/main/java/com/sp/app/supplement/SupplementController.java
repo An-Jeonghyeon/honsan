7a6aebdb4ef29b05a2c0c3a93e2f859ab6bac604
jiyoung <jiyoung@DESKTOP-NRGAMA5>
2020-12-16 21:14:51 +0900

    layout_login수정하고 영양제도 추가함

--- final/src/main/java/com/sp/app/supplement/SupplementController.java
+++ final/src/main/java/com/sp/app/supplement/SupplementController.java
@@ -1,8 +1,15 @@
 package com.sp.app.supplement;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("supplement.supplementController")
+@RequestMapping("/supplement/*")
 public class SupplementController {
-
+	
+	@RequestMapping("list")
+	public String list() throws Exception {
+		
+		return ".supplement.list";
+	}
 }
