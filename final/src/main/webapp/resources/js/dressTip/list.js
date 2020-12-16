230bd621fd6434c4f77130eb8ffc5620363284a6
ikyoungtae <ikyoungtae@macbook-pro>
2020-12-16 17:58:56 +0900

    이경

--- final/src/main/webapp/resources/js/dressTip/list.js
+++ final/src/main/webapp/resources/js/dressTip/list.js
@@ -1,11 +1,10 @@
 $(function(){
-    
     $(".dress-TipMenu1 > a").click(function(){
-        $(".dress-TipMenuBody1").css("background-color","rgb(201, 201, 201)");
-        $(".dress-TipMenuBody2").css("background-color","rgb(233, 233, 233)")
+        $(".dress-TipMenuBody1").css("background-color","#ededed80");
+        $(".dress-TipMenuBody2").css("background-color","rgba(192, 226, 255, 0.523)")
     })
     $(".dress-TipMenu2 > a").click(function(){
-        $(".dress-TipMenuBody2").css("background-color","rgb(201, 201, 201)")
-        $(".dress-TipMenuBody1").css("background-color","rgb(233, 233, 233)");
+        $(".dress-TipMenuBody2").css("background-color","#ededed80")
+        $(".dress-TipMenuBody1").css("background-color","rgba(192, 226, 255, 0.523)")
     })
  })
