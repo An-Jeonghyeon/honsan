58bca1affa8e03880ea31ef098320f74aecf3950
Apple <Apple@DESKTOP-KN2UT03>
2020-12-21 15:49:33 +0900

    보안 - 닿

---
+++ final/src/main/java/com/sp/app/room/SecurityController.java
@@ -0,0 +1,17 @@
+package com.sp.app.room;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("room.securityController")
+@RequestMapping("/room/*")
+public class SecurityController {
+	
+	@RequestMapping(value="security", method=RequestMethod.GET)
+	public String main() {
+		
+		return ".room.security";
+	}
+
+}
