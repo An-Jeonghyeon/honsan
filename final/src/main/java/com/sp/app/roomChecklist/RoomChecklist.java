71483f2ee19d091c71206cad93eefbcc246ad031
Apple <Apple@172.16.11.204>
2020-12-28 02:54:00 +0900

    푸쉬

---
+++ final/src/main/java/com/sp/app/roomChecklist/RoomChecklist.java
@@ -0,0 +1,54 @@
+package com.sp.app.roomChecklist;
+
+public class RoomChecklist {
+	private int num;
+	private String userId;
+	private String memo;
+	private String item;
+	private String register_date;
+	
+	private String ck;
+	
+	
+	
+	
+	public String getCk() {
+		return ck;
+	}
+	public void setCk(String ck) {
+		this.ck = ck;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public String getRegister_date() {
+		return register_date;
+	}
+	public void setRegister_date(String register_date) {
+		this.register_date = register_date;
+	}
+	
+	
+	
+}
