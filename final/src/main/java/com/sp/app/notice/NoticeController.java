e9c7e903239c16ffdf46683a2b78a8b0727568a8
jiyoung <jiyoung@DESKTOP-NRGAMA5>
2021-01-02 15:42:20 +0900

    노티스 수정

--- final/src/main/java/com/sp/app/notice/NoticeController.java
+++ final/src/main/java/com/sp/app/notice/NoticeController.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
