f7538a4ec464238d9588e876073085a17e967c26
ikyoungtae <ikyoungtae@macbook-pro>
2021-01-04 18:41:28 +0900

    asdsa

--- final/src/main/java/com/sp/app/dressTipClean/DressTipCleanService.java
+++ final/src/main/java/com/sp/app/dressTipClean/DressTipCleanService.java
@@ -8,7 +8,7 @@ public interface DressTipCleanService {
 	public List<DressTipClean> listDressTipClean(Map<String, Object> map) throws Exception;
 	public int dataCount(Map<String, Object> map);
 	public void updateHitCount(int num)throws Exception;
-	public  DressTipClean readDressTipClean(int num);
+	public DressTipClean readDressTipClean(int num);
 	public void deleteDressTipClean(int num, String userId);
 	public void updateDressTipClean(DressTipClean dto)throws Exception;
 }
