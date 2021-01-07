eafe26627f57858749272ef2c6847ca9e91221f9
jiyoung <jiyoung@192.168.219.101>
2021-01-07 14:58:59 +0900

    로고크기 수정

--- final/src/main/java/com/sp/app/healthStory/HealthStory.java
+++ final/src/main/java/com/sp/app/healthStory/HealthStory.java
@@ -1,5 +1,87 @@
 package com.sp.app.healthStory;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class HealthStory {
+	
+	private int num; 
+	private String userId; 
+	private String content; 
+	private String register_date;
+	private int enabled;  //이건 안쓰는데 일단 넣어둠
+
+	//다중파일
+	private int fileNum; 
+	private String saveFile;
+	
+	private List<MultipartFile> upload;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRegister_date() {
+		return register_date;
+	}
+
+	public void setRegister_date(String register_date) {
+		this.register_date = register_date;
+	}
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+	public int getFileNum() {
+		return fileNum;
+	}
+
+	public void setFileNum(int fileNum) {
+		this.fileNum = fileNum;
+	}
+
+	public String getSaveFile() {
+		return saveFile;
+	}
+
+	public void setSaveFile(String saveFile) {
+		this.saveFile = saveFile;
+	}
+
+	public List<MultipartFile> getUpload() {
+		return upload;
+	}
+
+	public void setUpload(List<MultipartFile> upload) {
+		this.upload = upload;
+	} 
+
+	
 
 }
--- final/src/main/java/com/sp/app/healthStory/HealthStoryServiceImpl.java
+++ final/src/main/java/com/sp/app/healthStory/HealthStoryServiceImpl.java
@@ -1,9 +1,75 @@
 package com.sp.app.healthStory;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.sp.app.room.Room;
+import com.sp.app.supplement.Supplement;
 
 @Service("today.TodayService")
 public class HealthStoryServiceImpl implements HealthStoryService {
+
+	@Override
+	public void insertHealthStory(HealthStory dto, String pathname) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteHealthStory(int num, String pathname, String userId) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateHealthStory(HealthStory dto, String pathname) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Supplement readHealthStory(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Supplement> listHealthStory(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int dataCount(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void insertFile(HealthStory dto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Room> listFile(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Room readFile(int fileNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteFile(Map<String, Object> map) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 
 }
